a9529740c43dbd5d0c28f4aa0ecae3f1eebf3c56
Annabel <annabel.rch31@gmail.com>
2022-02-19 18:38:09 -0500

    deleted unnecessay files, tried importing html from joely

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Calgary hacks 2022
+
+Cal hacks project for Kids4Kars
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/FirstPage.js
@@ -0,0 +1,35 @@
+import React from "react"
+import "./styles.css"
+
+export default function Desktop3() {
+  return (
+    <div className="desktop-3 flex-col-hstart-vstart clip-contents">
+      <div className="component-1 flex-col-hstart-vstart">
+        <p className="txt-611">
+          To <span className="txt-6112">Do</span>
+        </p>
+      </div>
+      <div className="rectangle-10" />
+      <p className="txt-9105">Roast Board</p>
+      <div className="group-596 flex-row-vend">
+        <div className="group-949">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/XiDp39ii3u7E5J9deLvXqH-13%3A66?alt=media&token=32b83602-d547-41bf-bb28-97212f984ea5"
+            alt="image not found"
+            className="arrow-2"
+          />
+        </div>
+        <div className="group-1034">
+          <p className="txt-122">hit THREE kids </p>
+          <p className="txt-858">hit TWO kids </p>
+          <p className="txt-130">hit a kid </p>
+          <div className="rectangle-13" />
+          <div className="rectangle-12" />
+          <div className="rectangle-11" />
+          <div className="rectangle-6 flex-col-hstart-vstart"></div>
+        </div>
+        <div className="rectangle-101" />
+      </div>
+    </div>
+  )
+}
