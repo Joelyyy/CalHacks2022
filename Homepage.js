b535f248e6088c275e543eab5111d97ad161cac8
Kaitlyn015 <100046573+Kaitlyn015@users.noreply.github.com>
2022-02-19 23:49:12 -0500

    Update Homepage.js

--- Homepage.js
+++ Homepage.js
@@ -5,37 +5,44 @@ export default function Slide1691() {
   return (
     <div className="slide-1691 flex-col-hstart-vstart clip-contents">
       <div className="flex-row">
-        <div className="group-359">
+        <div className="group-271">
           <img
-            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/FWdBg2dbyRH94U36n0q0zE-2%3A9?alt=media&token=01bf78bc-085d-4493-a485-19fc4fc49ed0"
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/FWdBg2dbyRH94U36n0q0zE-2%3A9?alt=media&token=b9893905-c176-4412-96b3-683046332f01"
             alt="image not found"
             className="polygon-1"
           />
         </div>
-        <div className="group-541 flex-col">
-          <div className="group-232 flex-col-hcenter">
-            <div className="group-421 flex-row">
-              <p className="txt-307">Sun</p>
-              <p className="txt-307">Mon</p>
-              <p className="txt-307">Tues</p>
-              <p className="txt-307">Wed</p>
-              <p className="txt-816">Thurs</p>
-              <p className="txt-307">Fri</p>
-              <p className="txt-396">Sat</p>
+        <div className="group-165 flex-col">
+          <div className="group-488 flex-col-hcenter">
+            <div className="group-175 flex-row">
+              <p className="txt-5810">Sun</p>
+              <p className="txt-5810">Mon</p>
+              <p className="txt-5810">Tues</p>
+              <p className="txt-5810">Wed</p>
+              <p className="txt-199">Thurs</p>
+              <p className="txt-5810">Fri</p>
+              <p className="txt-8102">Sat</p>
             </div>
             <div className="rectangle-7" />
           </div>
-          <div className="group-744">
-            <div className="group-282">
-              <p className="txt-126">Whiteboard</p>
+          <div className="group-686 flex-col-hcenter">
+            <div className="group-883">
+              <p className="txt-996">Whiteboard</p>
             </div>
+            <div className="rectangle-9" />
           </div>
         </div>
-        <div className="group-873 flex-col-hcenter">
-          <div className="group-333">
-            <p className="txt-239">To Do</p>
+        <div className="group-576 flex-col-hcenter">
+          <div className="group-927">
+            <p className="txt-9410">To Do</p>
           </div>
-          <div className="rectangle-8" />
+          <div className="group-965">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/FWdBg2dbyRH94U36n0q0zE-5%3A43?alt=media&token=c8f0844f-5fd0-4b9e-b24d-64909f31a74c"
+              alt="image not found"
+              className="polygon-2"
+            />
+          </div>
         </div>
       </div>
     </div>
