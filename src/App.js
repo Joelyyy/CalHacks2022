84840e957ff5cd7117f5cf6d686034fd1b73658d
Annabel <annabel.rch31@gmail.com>
2022-02-20 04:37:58 -0500

    rest of the stuff

--- src/App.js
+++ src/App.js
@@ -3,12 +3,69 @@ import Homepage from './Homepage'
 import Calender from './calender'
 import Groceries from './groceries';
 import Billing from './billing';
+import Notes from './Notes';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-        <Groceries/>
+        <Homepage/>
   )
 }
+
+// const Header = () => {
+// 	return (
+// 		<div>
+// 				<p>Header</p>
+// 		</div>
+// 	)
+// };
+
+// const homepage = () => {
+// 	return (
+// 		<div>
+// 				<Header/>
+// 				<Link to='/welcome'></Link>
+// 		</div>
+// 	)
+// };
+
+// const calender = () => {
+// 	return (
+// 		<div>
+// 				<Header/>
+// 				<Link to='/calender'></Link>
+// 		</div>
+// 	)
+// };
+
+// const groceries = () => {
+// 	return (
+// 		<div>
+// 				<Header/>
+// 				<Link to='/groceries'></Link>
+// 		</div>
+// 	)
+// };
+
+// const billing = () => {
+// 	return (
+// 		<div>
+// 				<Header/>
+// 				<Link to='/billing'></Link>
+// 		</div>
+// 	)
+// };
+
+// const notes = () => {
+// 	return (
+// 		<div>
+// 				<Header/>
+// 				<Link to='/notes'></Link>
+// 		</div>
+// 	)
+// };
+
+// export {homepage, calender, groceries, billing, notes } ;
 
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
+// import {homepage, calender, groceries, billing, notes} from './App'
+// import Homepage from "./Homepage"
+// import Calender from "./calender"
+// import Groceries from "./groceries"
+// import Billing from "./billing"
+// import Notes from "./Notes"
 
 
 ReactDOM.render(
@@ -9,5 +16,15 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+{/* <Router>
+<Routes>
+  <Route exact path="/welcome" element={<Homepage/>}/>
+  <Route exact path="/calender" element={<Calender/>}/>
+  <Route exact path="/groceries"element={<Groceries/>}/>
+  <Route exact path="/billing" element={<Billing/>}/>
+  <Route exact path="/notes" element={<Notes/>}/>
+</Routes>
+</Router>, */}
 
 
