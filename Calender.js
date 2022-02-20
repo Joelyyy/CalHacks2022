a413de85e41f955df92749b5aae4a4458ca61b48
Joelyyy <100046599+Joelyyy@users.noreply.github.com>
2022-02-20 00:07:20 -0500

    Create Calender.js

---
+++ Calender.js
@@ -0,0 +1,33 @@
+import React from "react"
+import "./styles.css"
+
+export default function Calender() {
+  return (
+    <div className="calender flex-col-hstart-vstart clip-contents">
+      <div className="group-625 flex-row">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/HV5ZWanpZa3cj2dMAcnucB-2%3A39?alt=media&token=eceee9d2-db36-4b2c-8512-0f93e9225429"
+          alt="image not found"
+          className="rectangle-36"
+        />
+        <div className="group-857 flex-col">
+          <p className="txt-4410">February 2022</p>
+          <div className="group-166 flex-row">
+            <p className="txt-373">S</p>
+            <p className="txt-373">M</p>
+            <p className="txt-988">T</p>
+            <p className="txt-037">W</p>
+            <p className="txt-373">T</p>
+            <p className="txt-532">F</p>
+            <p className="txt-177">S</p>
+          </div>
+        </div>
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/HV5ZWanpZa3cj2dMAcnucB-3%3A109?alt=media&token=c41743ca-59dd-4b82-b53d-2658ff16e258"
+        alt="image not found"
+        className="vector-1"
+      />
+    </div>
+  )
+}
