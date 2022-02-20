2edc984ad970087d9bc7551ebd8120b004d27671
leaelea <100046597+leaelea@users.noreply.github.com>
2022-02-20 01:11:18 -0500

    Update Billing.js

--- Billing.js
+++ Billing.js
@@ -4,32 +4,37 @@ import "./styles.css"
 export default function Slide1692() {
   return (
     <div className="slide-1692 flex-col-hstart-vstart clip-contents">
-      <div className="flex-row-vend">
-        <div className="group-343">
+      <div className="flex-row">
+        <div className="group-465">
           <img
-            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/MnrCvJE6dcXE7ARJHLLyJ1-5%3A11?alt=media&token=94cdd100-8210-427c-b038-12675c9dd075"
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/MnrCvJE6dcXE7ARJHLLyJ1-5%3A11?alt=media&token=caf6abc2-11d6-46b9-9efc-2d6980e1e65d"
             alt="image not found"
             className="polygon-2"
           />
         </div>
-        <div className="rectangle-41" />
-      </div>
-      <div className="group-5">
-        <div className="group-3">
-          <p className="txt-269 flex-hcenter">Billing</p>
+        <div className="group-6 flex-col-hcenter">
+          <div className="group-3">
+            <p className="txt-763 flex-hcenter">Billing</p>
+          </div>
+          <div className="group-460">
+            <div className="group-5 flex-col">
+              <div className="group-752 flex-row">
+                <p className="txt-824 flex-hcenter">Expense</p>
+                <p className="txt-5310 flex-hcenter">Amount</p>
+                <p className="txt-671 flex-hcenter">Date</p>
+                <p className="txt-614 flex-hcenter">Paid By</p>
+              </div>
+              <div className="flex-row">
+                <div className="rectangle-44" />
+                <div className="rectangle-45" />
+                <div className="rectangle-46" />
+                <div className="rectangle-47" />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="group-4">
-          <div className="rectangle-44" />
-          <div className="rectangle-45" />
-          <div className="rectangle-46" />
-          <div className="rectangle-47" />
-          <div className="rectangle-48" />
-        </div>
-        <p className="txt-712 flex-hcenter">Expense</p>
-        <p className="txt-795 flex-hcenter">Amount</p>
-        <p className="txt-5106 flex-hcenter">Date</p>
-        <p className="txt-277 flex-hcenter">Paid By</p>
       </div>
     </div>
   )
 }
+
