d8426bf6e8e3e7c8adab913aee6ca3bc2ca4ed84
leaelea <100046597+leaelea@users.noreply.github.com>
2022-02-20 01:27:39 -0500

    Create CalendarPopup.js

---
+++ CalendarPopup.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./styles.css"
+
+export default function Slide1693() {
+  return (
+    <div className="slide-1693 flex-col-hstart-vstart clip-contents">
+      <div className="group-463 flex-col">
+        <div className="group-1041">
+          <p className="txt-759">Add Task</p>
+        </div>
+        <div className="group-042 flex-row-vcenter">
+          <div className="group-7101">
+            <p className="txt-759">Add Time</p>
+          </div>
+          <div className="ellipse-5" />
+          <p className="txt-887">AM</p>
+          <div className="ellipse-5" />
+          <p className="txt-759">PM</p>
+        </div>
+        <div className="group-007">
+          <p className="txt-904">Save</p>
+        </div>
+      </div>
+    </div>
+  )
+}
