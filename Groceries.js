9328520195a1192cfb6451e107453eb6de95b682
leaelea <100046597+leaelea@users.noreply.github.com>
2022-02-19 23:44:27 -0500

    Create Groceries.js

---
+++ Groceries.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "./styles.css"
+
+export default function Desktop1() {
+  return (
+    <div className="desktop-1 flex-col-hstart-vstart clip-contents">
+      <div className="flex-row">
+        <div className="rectangle-39" />
+        <div className="rectangle-38" />
+        <div className="rectangle-37" />
+      </div>
+      <div className="group-1">
+        <p className="txt-666">Groceries</p>
+      </div>
+      <div className="group-2">
+        <p className="txt-666">Chores</p>
+      </div>
+    </div>
+  )
+}
