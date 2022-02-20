0249dbc8a4284ab18f95bf7532a941f6cbcf400f
Kaitlyn015 <100046573+Kaitlyn015@users.noreply.github.com>
2022-02-19 21:53:27 -0500

    Update Homepage.js

--- Homepage.js
+++ Homepage.js
@@ -7,25 +7,25 @@ export default function Homepage() {
       <div className="flex-col-hstart-vstart">
         <div className="homepage flex-col-hstart-vstart clip-contents">
           <img
-            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/ssdj1Ax7HPa0RfTonKe0Zq-2%3A278?alt=media&token=df8761da-9cb9-4950-9287-fdf4ed3d67b2"
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/XiDp39ii3u7E5J9deLvXqH-18%3A553?alt=media&token=be9bdd69-9259-45d3-8d97-f9c83f091c21"
             alt="image not found"
             className="group-29"
           />
           <div className="flex-row-vcenter">
-            <div className="group-2910 flex-row">
-              <div className="group-442 flex-col-hcenter">
+            <div className="group-715 flex-row">
+              <div className="group-1013 flex-col-hcenter">
                 <div className="group-20">
                   <div className="group-3">
                     <div className="group-4">
                       <div className="weekday-calender flex-col-hcenter-vstart">
-                        <div className="group-731 flex-row">
-                          <p className="txt-219 flex-hcenter">Sun</p>
-                          <p className="txt-982 flex-hcenter">Mon</p>
-                          <p className="txt-748 flex-hcenter">Tues</p>
-                          <p className="txt-5100 flex-hcenter">Wed</p>
-                          <p className="txt-1063 flex-hcenter">Thur</p>
-                          <p className="txt-433 flex-hcenter">Fri</p>
-                          <p className="txt-630 flex-hcenter">Sun</p>
+                        <div className="group-582 flex-row">
+                          <p className="txt-1056 flex-hcenter">Sun</p>
+                          <p className="txt-989 flex-hcenter">Mon</p>
+                          <p className="txt-055 flex-hcenter">Tues</p>
+                          <p className="txt-1066 flex-hcenter">Wed</p>
+                          <p className="txt-7110 flex-hcenter">Thur</p>
+                          <p className="txt-125 flex-hcenter">Fri</p>
+                          <p className="txt-488 flex-hcenter">Sat</p>
                         </div>
                         <div className="rectangle-2" />
                       </div>
@@ -36,31 +36,31 @@ export default function Homepage() {
                 <div className="rectangle-15" />
               </div>
               <div className="group-28">
-                <div className="group-295">
+                <div className="group-464">
                   <div className="rectangle-6 flex-col-hstart-vstart"></div>
                 </div>
-                <div className="group-748">
+                <div className="group-570">
                   <div className="component-1 flex-col-hstart-vstart">
-                    <p className="txt-075">
-                      To <span className="txt-0752">Do</span>
+                    <p className="txt-1026">
+                      To <span className="txt-10262">Do</span>
                     </p>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="group-4421 flex-col-hcenter">
+            <div className="group-10131 flex-col-hcenter">
               <div className="group-20">
                 <div className="group-3">
                   <div className="group-4">
                     <div className="weekday-calender flex-col-hcenter-vstart">
-                      <div className="group-731 flex-row">
-                        <p className="txt-219 flex-hcenter">Sun</p>
-                        <p className="txt-982 flex-hcenter">Mon</p>
-                        <p className="txt-748 flex-hcenter">Tues</p>
-                        <p className="txt-5100 flex-hcenter">Wed</p>
-                        <p className="txt-1063 flex-hcenter">Thur</p>
-                        <p className="txt-433 flex-hcenter">Fri</p>
-                        <p className="txt-630 flex-hcenter">Sun</p>
+                      <div className="group-582 flex-row">
+                        <p className="txt-1056 flex-hcenter">Sun</p>
+                        <p className="txt-989 flex-hcenter">Mon</p>
+                        <p className="txt-055 flex-hcenter">Tues</p>
+                        <p className="txt-1066 flex-hcenter">Wed</p>
+                        <p className="txt-7110 flex-hcenter">Thur</p>
+                        <p className="txt-125 flex-hcenter">Fri</p>
+                        <p className="txt-488 flex-hcenter">Sat</p>
                       </div>
                       <div className="rectangle-2" />
                     </div>
@@ -71,7 +71,7 @@ export default function Homepage() {
               <div className="rectangle-15" />
             </div>
           </div>
-          <p className="txt-273">Whiteboard</p>
+          <p className="txt-495">Whiteboard</p>
           <div className="group-27">
             <div className="rectangle-10" />
             <div className="rectangle-8" />
@@ -81,5 +81,7 @@ export default function Homepage() {
     </div>
   )
 }
+
+
 
 
