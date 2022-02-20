f796785b86b3b753a78e87cc0e388bc9cdd0efa0
Kaitlyn015 <100046573+Kaitlyn015@users.noreply.github.com>
2022-02-19 22:47:07 -0500

    Update Homepage.js

--- Homepage.js
+++ Homepage.js
@@ -3,85 +3,55 @@ import "./styles.css"
 
 export default function Homepage() {
   return (
-    <div className="homepage-2 flex-col-hstart-vstart">
-      <div className="flex-col-hstart-vstart">
-        <div className="homepage flex-col-hstart-vstart clip-contents">
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/XiDp39ii3u7E5J9deLvXqH-18%3A553?alt=media&token=be9bdd69-9259-45d3-8d97-f9c83f091c21"
-            alt="image not found"
-            className="group-29"
-          />
-          <div className="flex-row-vcenter">
-            <div className="group-715 flex-row">
-              <div className="group-1013 flex-col-hcenter">
-                <div className="group-20">
-                  <div className="group-3">
-                    <div className="group-4">
-                      <div className="weekday-calender flex-col-hcenter-vstart">
-                        <div className="group-582 flex-row">
-                          <p className="txt-1056 flex-hcenter">Sun</p>
-                          <p className="txt-989 flex-hcenter">Mon</p>
-                          <p className="txt-055 flex-hcenter">Tues</p>
-                          <p className="txt-1066 flex-hcenter">Wed</p>
-                          <p className="txt-7110 flex-hcenter">Thur</p>
-                          <p className="txt-125 flex-hcenter">Fri</p>
-                          <p className="txt-488 flex-hcenter">Sat</p>
-                        </div>
-                        <div className="rectangle-2" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="rectangle-17" />
-                <div className="rectangle-15" />
-              </div>
-              <div className="group-28">
-                <div className="group-464">
-                  <div className="rectangle-6 flex-col-hstart-vstart"></div>
-                </div>
-                <div className="group-570">
-                  <div className="component-1 flex-col-hstart-vstart">
-                    <p className="txt-1026">
-                      To <span className="txt-10262">Do</span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="group-10131 flex-col-hcenter">
-              <div className="group-20">
-                <div className="group-3">
-                  <div className="group-4">
-                    <div className="weekday-calender flex-col-hcenter-vstart">
-                      <div className="group-582 flex-row">
-                        <p className="txt-1056 flex-hcenter">Sun</p>
-                        <p className="txt-989 flex-hcenter">Mon</p>
-                        <p className="txt-055 flex-hcenter">Tues</p>
-                        <p className="txt-1066 flex-hcenter">Wed</p>
-                        <p className="txt-7110 flex-hcenter">Thur</p>
-                        <p className="txt-125 flex-hcenter">Fri</p>
-                        <p className="txt-488 flex-hcenter">Sat</p>
-                      </div>
-                      <div className="rectangle-2" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="rectangle-17" />
-              <div className="rectangle-15" />
-            </div>
-          </div>
-          <p className="txt-495">Whiteboard</p>
-          <div className="group-27">
+    <div className="homepage flex-col-hstart-vstart clip-contents">
+      <div className="flex-row-vend">
+        <div className="group-043">
+          <div className="group-834 flex-row">
+            <div className="rectangle-6" />
+            <div className="rectangle-7" />
+            <div className="rectangle-8" />
+            <div className="rectangle-6" />
             <div className="rectangle-10" />
             <div className="rectangle-8" />
+            <div className="rectangle-12" />
+          </div>
+          <p className="txt-617">Sat</p>
+          <p className="txt-533">Fri</p>
+          <p className="txt-448">Thurs</p>
+          <p className="txt-728">Weds</p>
+          <p className="txt-353">Tues</p>
+          <p className="txt-234">Mon</p>
+          <p className="txt-075">Sun</p>
+        </div>
+        <div className="group-223">
+          <div className="group-1018">
+            <p className="txt-1040">Whiteboard</p>
           </div>
         </div>
+        <div className="group-239">
+          <p className="txt-929">To Do</p>
+        </div>
+      </div>
+      <div className="group-043">
+        <div className="group-834 flex-row">
+          <div className="rectangle-6" />
+          <div className="rectangle-7" />
+          <div className="rectangle-8" />
+          <div className="rectangle-6" />
+          <div className="rectangle-10" />
+          <div className="rectangle-8" />
+          <div className="rectangle-12" />
+        </div>
+        <p className="txt-617">Sat</p>
+        <p className="txt-533">Fri</p>
+        <p className="txt-448">Thurs</p>
+        <p className="txt-728">Weds</p>
+        <p className="txt-353">Tues</p>
+        <p className="txt-234">Mon</p>
+        <p className="txt-075">Sun</p>
       </div>
     </div>
   )
 }
-
-
 
 
