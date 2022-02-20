8951b413b7ea67a8f5c9050c4cac85eb0045f759
Kaitlyn015 <100046573+Kaitlyn015@users.noreply.github.com>
2022-02-19 20:09:48 -0500

    Create Homepage.js

---
+++ Homepage.js
@@ -0,0 +1,67 @@
+import React from "react"
+import "./styles.css"
+
+export default function Homepage() {
+  return (
+    <div className="homepage flex-col-hstart-vstart clip-contents">
+      <div className="flex-row">
+        <div className="group-840">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/XiDp39ii3u7E5J9deLvXqH-13%3A46?alt=media&token=71f4886b-79fb-4638-8bfa-f393db1db153"
+            alt="image not found"
+            className="arrow-2"
+          />
+        </div>
+        <div className="group-569 flex-col-hcenter">
+          <div className="group-633">
+            <div className="group-3">
+              <div className="group-4">
+                <div className="weekday-calender flex-col-hcenter-vstart">
+                  <div className="group-512 flex-row">
+                    <p className="txt-426 flex-hcenter">Sun</p>
+                    <p className="txt-554 flex-hcenter">Mon</p>
+                    <p className="txt-926 flex-hcenter">Tues</p>
+                    <p className="txt-529 flex-hcenter">Wed</p>
+                    <p className="txt-976 flex-hcenter">Thur</p>
+                    <p className="txt-815 flex-hcenter">Fri</p>
+                    <p className="txt-262 flex-hcenter">Sun</p>
+                  </div>
+                  <div className="rectangle-2" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="group-1051">
+            <p className="txt-487">Roast Board</p>
+            <div className="rectangle-10" />
+          </div>
+        </div>
+        <div className="group-460">
+          <div className="component-1 flex-col-hstart-vstart">
+            <p className="txt-259">
+              To <span className="txt-2592">Do</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="group-488">
+        <div className="rectangle-8" />
+      </div>
+      <div className="group-7101">
+        <div className="group-10">
+          <div className="rectangle-28" />
+          <div className="rectangle-29" />
+        </div>
+        <p className="txt-752">hit THREE kids </p>
+        <p className="txt-1060">hit TWO kids </p>
+        <p className="txt-994">hit a kid </p>
+        <div className="rectangle-13" />
+        <div className="rectangle-12" />
+        <div className="rectangle-11" />
+        <div className="rectangle-6 flex-col-hstart-vstart">
+          <div className="rectangle-291" />
+        </div>
+      </div>
+    </div>
+  )
+}
