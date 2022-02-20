e3e53f7adf614b40ee1255de4553e50f96131cc1
Kaitlyn015 <100046573+Kaitlyn015@users.noreply.github.com>
2022-02-19 23:39:33 -0500

    Update Homepage.js

--- Homepage.js
+++ Homepage.js
@@ -5,36 +5,37 @@ export default function Slide1691() {
   return (
     <div className="slide-1691 flex-col-hstart-vstart clip-contents">
       <div className="flex-row">
-        <div className="group-1084">
+        <div className="group-359">
           <img
-            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/FWdBg2dbyRH94U36n0q0zE-2%3A9?alt=media&token=08fe0242-3c5b-4d85-8319-4b5d5934ba27"
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/FWdBg2dbyRH94U36n0q0zE-2%3A9?alt=media&token=01bf78bc-085d-4493-a485-19fc4fc49ed0"
             alt="image not found"
             className="polygon-1"
           />
         </div>
-        <div className="group-512 flex-col">
-          <div className="group-316 flex-col-hcenter">
-            <div className="group-288 flex-row">
-              <p className="txt-684">Sun</p>
-              <p className="txt-684">Mon</p>
-              <p className="txt-684">Tues</p>
-              <p className="txt-684">Wed</p>
-              <p className="txt-296">Thurs</p>
-              <p className="txt-684">Fri</p>
-              <p className="txt-574">Sat</p>
+        <div className="group-541 flex-col">
+          <div className="group-232 flex-col-hcenter">
+            <div className="group-421 flex-row">
+              <p className="txt-307">Sun</p>
+              <p className="txt-307">Mon</p>
+              <p className="txt-307">Tues</p>
+              <p className="txt-307">Wed</p>
+              <p className="txt-816">Thurs</p>
+              <p className="txt-307">Fri</p>
+              <p className="txt-396">Sat</p>
             </div>
             <div className="rectangle-7" />
           </div>
-          <div className="group-619">
-            <div className="group-034">
-              <p className="txt-4108">Whiteboard</p>
+          <div className="group-744">
+            <div className="group-282">
+              <p className="txt-126">Whiteboard</p>
             </div>
           </div>
         </div>
-        <div className="group-344">
-          <div className="group-376">
-            <p className="txt-310">To Do</p>
+        <div className="group-873 flex-col-hcenter">
+          <div className="group-333">
+            <p className="txt-239">To Do</p>
           </div>
+          <div className="rectangle-8" />
         </div>
       </div>
     </div>
