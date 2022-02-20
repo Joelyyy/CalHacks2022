dc83565736badbc2dcbb3614b91339f649a3b252
Kaitlyn015 <100046573+Kaitlyn015@users.noreply.github.com>
2022-02-19 22:52:54 -0500

    Update Homepage.js

--- Homepage.js
+++ Homepage.js
@@ -1,57 +1,17 @@
 import React from "react"
 import "./styles.css"
 
-export default function Homepage() {
+export default function Slide1691() {
   return (
-    <div className="homepage flex-col-hstart-vstart clip-contents">
-      <div className="flex-row-vend">
-        <div className="group-043">
-          <div className="group-834 flex-row">
-            <div className="rectangle-6" />
-            <div className="rectangle-7" />
-            <div className="rectangle-8" />
-            <div className="rectangle-6" />
-            <div className="rectangle-10" />
-            <div className="rectangle-8" />
-            <div className="rectangle-12" />
-          </div>
-          <p className="txt-617">Sat</p>
-          <p className="txt-533">Fri</p>
-          <p className="txt-448">Thurs</p>
-          <p className="txt-728">Weds</p>
-          <p className="txt-353">Tues</p>
-          <p className="txt-234">Mon</p>
-          <p className="txt-075">Sun</p>
+    <div className="slide-1691 flex-col-hstart-vstart clip-contents">
+      <div className="flex-row">
+        <div className="rectangle-1" />
+        <div className="group-956 flex-col">
+          <div className="rectangle-2" />
+          <div className="rectangle-3" />
         </div>
-        <div className="group-223">
-          <div className="group-1018">
-            <p className="txt-1040">Whiteboard</p>
-          </div>
-        </div>
-        <div className="group-239">
-          <p className="txt-929">To Do</p>
-        </div>
-      </div>
-      <div className="group-043">
-        <div className="group-834 flex-row">
-          <div className="rectangle-6" />
-          <div className="rectangle-7" />
-          <div className="rectangle-8" />
-          <div className="rectangle-6" />
-          <div className="rectangle-10" />
-          <div className="rectangle-8" />
-          <div className="rectangle-12" />
-        </div>
-        <p className="txt-617">Sat</p>
-        <p className="txt-533">Fri</p>
-        <p className="txt-448">Thurs</p>
-        <p className="txt-728">Weds</p>
-        <p className="txt-353">Tues</p>
-        <p className="txt-234">Mon</p>
-        <p className="txt-075">Sun</p>
+        <div className="rectangle-4" />
       </div>
     </div>
   )
 }
-
-
