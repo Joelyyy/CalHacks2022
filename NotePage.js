625ca73838e7f1469bb96f53b7758ca5526d90ed
Kaitlyn015 <100046573+Kaitlyn015@users.noreply.github.com>
2022-02-20 01:06:18 -0500

    Create NotePage.js

---
+++ NotePage.js
@@ -0,0 +1,36 @@
+import React from "react"
+import "./styles.css"
+
+export default function Slide1691() {
+  return (
+    <div className="slide-1691 flex-col-hstart-vstart clip-contents">
+      <div className="flex-row-vcenter">
+        <div className="group-754">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/CR9irTwggW22ohM46ieYIC-3%3A4?alt=media&token=f6bfa912-e2c0-4ff4-8541-002b5ed929d0"
+            alt="image not found"
+            className="polygon-1"
+          />
+        </div>
+        <div className="group-588 flex-col-hcenter">
+          <div className="group-412">
+            <p className="txt-635">Note</p>
+          </div>
+          <div className="rectangle-10" />
+        </div>
+        <div className="group-561 flex-col-hcenter">
+          <div className="group-062">
+            <p className="txt-635">Note</p>
+          </div>
+          <div className="rectangle-10" />
+        </div>
+        <div className="group-091 flex-col-hcenter">
+          <div className="group-062">
+            <p className="txt-635">Note</p>
+          </div>
+          <div className="rectangle-10" />
+        </div>
+      </div>
+    </div>
+  )
+}
