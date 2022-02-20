9b6a39349eccff3ead26aa9feaa99de9123a43cd
leaelea <100046597+leaelea@users.noreply.github.com>
2022-02-20 00:05:36 -0500

    Update Groceries.js

--- Groceries.js
+++ Groceries.js
@@ -1,20 +1,27 @@
 import React from "react"
 import "./styles.css"
 
-export default function Desktop1() {
+export default function Slide1691() {
   return (
-    <div className="desktop-1 flex-col-hstart-vstart clip-contents">
-      <div className="flex-row">
-        <div className="rectangle-39" />
+    <div className="slide-1691 flex-col-hstart-vstart clip-contents">
+      <div className="flex-row-vend">
+        <div className="group-428">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/MnrCvJE6dcXE7ARJHLLyJ1-2%3A40?alt=media&token=6fe86700-1887-46e9-a05d-c0cc588d726a"
+            alt="image not found"
+            className="polygon-1"
+          />
+        </div>
         <div className="rectangle-38" />
         <div className="rectangle-37" />
       </div>
-      <div className="group-1">
-        <p className="txt-666">Groceries</p>
-      </div>
       <div className="group-2">
-        <p className="txt-666">Chores</p>
+        <p className="txt-917">Groceries</p>
+      </div>
+      <div className="group-1">
+        <p className="txt-356">Chores</p>
       </div>
     </div>
   )
 }
+
