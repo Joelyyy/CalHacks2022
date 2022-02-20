b020c0745a11693a59985a758561bfb645252d50
Kaitlyn015 <100046573+Kaitlyn015@users.noreply.github.com>
2022-02-19 23:01:33 -0500

    Update Homepage.js

--- Homepage.js
+++ Homepage.js
@@ -5,12 +5,26 @@ export default function Slide1691() {
   return (
     <div className="slide-1691 flex-col-hstart-vstart clip-contents">
       <div className="flex-row">
-        <div className="rectangle-1" />
-        <div className="group-956 flex-col">
-          <div className="rectangle-2" />
-          <div className="rectangle-3" />
+        <div className="group-4610">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/FWdBg2dbyRH94U36n0q0zE-2%3A9?alt=media&token=64cf6b6c-6ece-4599-81cc-343b367778bf"
+            alt="image not found"
+            className="polygon-1"
+          />
         </div>
-        <div className="rectangle-4" />
+        <div className="group-810 flex-col">
+          <div className="rectangle-2" />
+          <div className="group-891">
+            <div className="group-979">
+              <p className="txt-603">Whiteboard</p>
+            </div>
+          </div>
+        </div>
+        <div className="group-023">
+          <div className="group-999">
+            <p className="txt-1066">To Do</p>
+          </div>
+        </div>
       </div>
     </div>
   )
