408ce3fd42d31a2db5081bc8cd4125537f598ef1
Kaitlyn015 <100046573+Kaitlyn015@users.noreply.github.com>
2022-02-19 23:14:35 -0500

    Update Homepage.js

--- Homepage.js
+++ Homepage.js
@@ -5,24 +5,35 @@ export default function Slide1691() {
   return (
     <div className="slide-1691 flex-col-hstart-vstart clip-contents">
       <div className="flex-row">
-        <div className="group-4610">
+        <div className="group-255">
           <img
-            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/FWdBg2dbyRH94U36n0q0zE-2%3A9?alt=media&token=64cf6b6c-6ece-4599-81cc-343b367778bf"
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/FWdBg2dbyRH94U36n0q0zE-2%3A9?alt=media&token=6723912e-11ce-4f34-b64f-a2d00acaf571"
             alt="image not found"
             className="polygon-1"
           />
         </div>
-        <div className="group-810 flex-col">
-          <div className="rectangle-2" />
-          <div className="group-891">
-            <div className="group-979">
-              <p className="txt-603">Whiteboard</p>
+        <div className="group-317 flex-col">
+          <div className="group-534 flex-col-hcenter">
+            <div className="group-133 flex-row">
+              <p className="txt-458">Sun</p>
+              <p className="txt-458">Mon</p>
+              <p className="txt-458">Tues</p>
+              <p className="txt-458">Wed</p>
+              <p className="txt-543">Thurs</p>
+              <p className="txt-458">Fri</p>
+              <p className="txt-542">Sat</p>
+            </div>
+            <div className="rectangle-7" />
+          </div>
+          <div className="group-380">
+            <div className="group-635">
+              <p className="txt-888">Whiteboard</p>
             </div>
           </div>
         </div>
-        <div className="group-023">
-          <div className="group-999">
-            <p className="txt-1066">To Do</p>
+        <div className="group-827">
+          <div className="group-238">
+            <p className="txt-218">To Do</p>
           </div>
         </div>
       </div>
