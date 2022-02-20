c9f40753bbfb8db3e4f167a389a724e0ed89cd25
Annabel <annabel.rch31@gmail.com>
2022-02-20 02:04:51 -0500

    Sidebar without css is in

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./Homepage.css"
+import Sidebar from './sidebar'
 
 
 class Homepage extends React.Component{
@@ -27,10 +28,37 @@ class Homepage extends React.Component{
       <div className="slide-1691 flex-col-hstart-vstart clip-contents">
           <div id="mySidenav" class="sidenav w3-animate-left">
             <a href="javascript:void(0)" class="closebtn" onClick={this.closeNav}>&times;</a>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Clients</a>
-            <a href="#">Contact</a>
+              <Sidebar />
+            <div className="slide-1691 flex-col-hstart-vstart clip-contents">
+      <div className="group-279 flex-col-hcenter">
+        <div className="group-516">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/rQ0rdGW58AQl8YLrXuzyJK-5%3A28?alt=media&token=da26a051-4ddd-4489-86f6-3eb23c07535d"
+            alt="image not found"
+            className="group-15"
+          />
+        </div>
+        <div className="group-871">
+          <p className="txt-276">Calender</p>
+        </div>
+        <div className="group-580">
+          <p className="txt-276">Groceries & Chores</p>
+        </div>
+        <div className="group-756">
+          <p className="txt-276">Bills</p>
+        </div>
+        <div className="group-910">
+          <p className="txt-276">Notes</p>
+        </div>
+        <div className="group-407">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/rQ0rdGW58AQl8YLrXuzyJK-5%3A16?alt=media&token=ace71b90-c888-4811-bde6-c81a7e09ac62"
+            alt="image not found"
+            className="polygon-1"
+          />
+        </div>
+      </div>
+    </div>
           </div>
         <div className="flex-row">
           <div className="group-271">
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,13 +1,37 @@
-/* add functionality for sidebar to slide in and out CHANGE THIS LATER*/
+import React from "react"
+// import "./sidebar.css"
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "500px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.body.style.backgroundColor = "white";
-  }
+export default function Sidebar() {
+  return (
+    <div className="slide-1691 flex-col-hstart-vstart clip-contents">
+      <div className="group-279 flex-col-hcenter">
+        <div className="group-516">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/rQ0rdGW58AQl8YLrXuzyJK-5%3A28?alt=media&token=da26a051-4ddd-4489-86f6-3eb23c07535d"
+            alt="image not found"
+            className="group-15"
+          />
+        </div>
+        <div className="group-871">
+          <p className="txt-276">Calender</p>
+        </div>
+        <div className="group-580">
+          <p className="txt-276">Groceries & Chores</p>
+        </div>
+        <div className="group-756">
+          <p className="txt-276">Bills</p>
+        </div>
+        <div className="group-910">
+          <p className="txt-276">Notes</p>
+        </div>
+        <div className="group-407">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/rQ0rdGW58AQl8YLrXuzyJK-5%3A16?alt=media&token=ace71b90-c888-4811-bde6-c81a7e09ac62"
+            alt="image not found"
+            className="polygon-1"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
