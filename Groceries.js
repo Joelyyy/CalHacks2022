737be5d13c935a71348df440c8c33ea0e3918fdc
Kaitlyn015 <100046573+Kaitlyn015@users.noreply.github.com>
2022-02-20 01:20:02 -0500

    Update Groceries.js

--- Groceries.js
+++ Groceries.js
@@ -4,22 +4,26 @@ import "./styles.css"
 export default function Slide1691() {
   return (
     <div className="slide-1691 flex-col-hstart-vstart clip-contents">
-      <div className="flex-row-vend">
-        <div className="group-428">
+      <div className="flex-row">
+        <div className="group-153">
           <img
-            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/MnrCvJE6dcXE7ARJHLLyJ1-2%3A40?alt=media&token=6fe86700-1887-46e9-a05d-c0cc588d726a"
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/MnrCvJE6dcXE7ARJHLLyJ1-15%3A3?alt=media&token=e0d73c38-9bb6-49c7-814e-d9ef4531b6fd"
             alt="image not found"
-            className="polygon-1"
+            className="polygon-3"
           />
         </div>
-        <div className="rectangle-38" />
-        <div className="rectangle-37" />
-      </div>
-      <div className="group-2">
-        <p className="txt-917">Groceries</p>
-      </div>
-      <div className="group-1">
-        <p className="txt-356">Chores</p>
+        <div className="group-158 flex-col-hcenter">
+          <div className="group-622">
+            <p className="txt-743">Groceries</p>
+          </div>
+          <div className="rectangle-53" />
+        </div>
+        <div className="group-5109 flex-col-hcenter">
+          <div className="group-323">
+            <p className="txt-278">Chores</p>
+          </div>
+          <div className="rectangle-53" />
+        </div>
       </div>
     </div>
   )
