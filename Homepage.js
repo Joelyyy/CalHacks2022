d505998e1db2aa375256005fa12917a5b0c29e06
Joelyyy <100046599+Joelyyy@users.noreply.github.com>
2022-02-19 20:32:12 -0500

    Update Homepage.js

--- Homepage.js
+++ Homepage.js
@@ -1,67 +1,67 @@
 import React from "react"
 import "./styles.css"
 
-export default function Frame15() {
+export default function Homepage() {
   return (
-    <div className="frame-15 flex-col-hstart-vstart">
+    <div className="homepage-2 flex-col-hstart-vstart">
       <div className="flex-col-hstart-vstart">
         <div className="flex-col-hstart-vstart">
           <div className="homepage flex-col-hstart-vstart clip-contents">
             <div className="flex-row">
-              <div className="group-1044">
+              <div className="group-855">
                 <img
-                  src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/XiDp39ii3u7E5J9deLvXqH-13%3A46?alt=media&token=27625c55-510c-4e39-a1c1-f5152640b39a"
+                  src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/XiDp39ii3u7E5J9deLvXqH-13%3A46?alt=media&token=7e06fe4b-2619-42f1-ac14-d88cce52c64a"
                   alt="image not found"
                   className="arrow-2"
                 />
               </div>
-              <div className="group-427 flex-col-hcenter">
-                <div className="group-493">
+              <div className="group-236 flex-col-hcenter">
+                <div className="group-9107">
                   <div className="group-3">
                     <div className="group-4">
                       <div className="weekday-calender flex-col-hcenter-vstart">
-                        <div className="group-058 flex-row">
-                          <p className="txt-761 flex-hcenter">Sun</p>
-                          <p className="txt-992 flex-hcenter">Mon</p>
-                          <p className="txt-630 flex-hcenter">Tues</p>
-                          <p className="txt-764 flex-hcenter">Wed</p>
-                          <p className="txt-067 flex-hcenter">Thur</p>
-                          <p className="txt-1091 flex-hcenter">Fri</p>
-                          <p className="txt-338 flex-hcenter">Sun</p>
+                        <div className="group-982 flex-row">
+                          <p className="txt-294 flex-hcenter">Sun</p>
+                          <p className="txt-836 flex-hcenter">Mon</p>
+                          <p className="txt-619 flex-hcenter">Tues</p>
+                          <p className="txt-223 flex-hcenter">Wed</p>
+                          <p className="txt-120 flex-hcenter">Thur</p>
+                          <p className="txt-265 flex-hcenter">Fri</p>
+                          <p className="txt-934 flex-hcenter">Sun</p>
                         </div>
                         <div className="rectangle-2" />
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className="group-795">
+                <div className="group-872">
                   <div className="rectangle-10" />
-                  <p className="txt-8210">Whiteboard</p>
+                  <p className="txt-0106">Whiteboard</p>
                 </div>
               </div>
-              <div className="group-557">
+              <div className="group-012">
                 <div className="component-1 flex-col-hstart-vstart">
-                  <p className="txt-248">
-                    To <span className="txt-2482">Do</span>
+                  <p className="txt-374">
+                    To <span className="txt-3742">Do</span>
                   </p>
                 </div>
               </div>
             </div>
-            <div className="group-818">
+            <div className="group-544">
               <div className="rectangle-6 flex-col-hstart-vstart"></div>
               <img
-                src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/XiDp39ii3u7E5J9deLvXqH-12%3A105?alt=media&token=47fc7ee3-ceba-480b-a28e-00d424521fd8"
+                src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/XiDp39ii3u7E5J9deLvXqH-12%3A105?alt=media&token=9351db24-3a61-4299-855c-ea77ea5f1846"
                 alt="image not found"
                 className="rectangle-19"
               />
               <img
-                src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/XiDp39ii3u7E5J9deLvXqH-17%3A7?alt=media&token=5c9dff9e-7b29-4305-bcd5-6c57d088c9f0"
+                src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/XiDp39ii3u7E5J9deLvXqH-17%3A7?alt=media&token=e928f6ec-79ea-4f25-ba10-833614f2a44e"
                 alt="image not found"
                 className="rectangle-28"
               />
               <div className="rectangle-29" />
             </div>
-            <div className="group-5102">
+            <div className="group-1008">
               <div className="rectangle-8" />
             </div>
           </div>
